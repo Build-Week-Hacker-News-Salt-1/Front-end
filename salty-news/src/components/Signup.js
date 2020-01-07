3096e9359bc2bbcecf75b9cc42f568f5cf7b3ae7
BenHall-7 <benjaminjahall@gmail.com>
2020-01-06 23:33:44 -0500

    Fix some inconsistent formatting

--- salty-news/src/components/Signup.js
+++ salty-news/src/components/Signup.js
@@ -7,48 +7,48 @@ const Signup = () => {
         username: "",
         password: "",
         email: ""
-      })
+    })
 
-      const handleSubmit = e => {
-        e.preventDefault();
-        axios
-            .post("http://localhost:3456/api/register", signupCredentials)
-            .then(res => {
-                console.log(res)
-            })
-            .catch(err =>
-                console.log(err)    
-            )
+    const handleSubmit = e => {
+    e.preventDefault();
+    axios
+        .post("http://localhost:3456/api/register", signupCredentials)
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err =>
+            console.log(err)    
+        )
     }
 
     return(
         <div className="login-container">
-        <h2 className="header-login">
-          Create Account
-        </h2>
-        <div className="login-form-main">
-          <form onSubmit={handleSubmit}>
-              <input
-                  type="text"
-                  name="username"
-                  value={signupCredentials.username}
-                  onChange={e => setSignupCredentials({username: e.target.value})}
-              />
-              <input
-                  type="text"
-                  name="password"
-                  value={signupCredentials.password}
-                  onChange={e => setSignupCredentials({password: e.target.value})}
-              />
-              <input
-                  type="text"
-                  name="email"
-                  value={signupCredentials.email}
-                  onChange={e => setSignupCredentials({email: e.target.value})}
-              />
-              <button>SignUp</button>
-          </form>
-        </div>
+            <h2 className="header-login">
+                Create Account
+            </h2>
+            <div className="login-form-main">
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        name="username"
+                        value={signupCredentials.username}
+                        onChange={e => setSignupCredentials({username: e.target.value})}
+                    />
+                    <input
+                        type="text"
+                        name="password"
+                        value={signupCredentials.password}
+                        onChange={e => setSignupCredentials({password: e.target.value})}
+                    />
+                    <input
+                        type="text"
+                        name="email"
+                        value={signupCredentials.email}
+                        onChange={e => setSignupCredentials({email: e.target.value})}
+                    />
+                    <button>SignUp</button>
+                </form>
+            </div>
         </div>
     )
 }
