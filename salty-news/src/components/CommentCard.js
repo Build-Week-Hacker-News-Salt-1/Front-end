d3dd540ba3add1d11da1c850924db1986168cd41
Veto Ramirez <vetorobert3@gmail.com>
2020-01-08 20:16:32 -0800

    Creating the comment card for logged in users

--- salty-news/src/components/CommentCard.js
+++ salty-news/src/components/CommentCard.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
 let StyledCard = styled.div`
     /*nothing yet*/
 `;;
 
 export default function CommentCard(props) {
-    return (
-        <StyledCard>
-            
-        </StyledCard>
-    )
+
+  const [comment, setComment] = useState({title: "", text: ""});
+
+  return (
+    <StyledCard>
+      <form>
+        <div>
+          <label>
+            title:
+            <input 
+              type='text' 
+              name='title'
+              value={title}
+            />
+          </label>
+        </div>
+      </form>
+    </StyledCard>
+  )
 }
