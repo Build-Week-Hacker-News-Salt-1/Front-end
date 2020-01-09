77eedd45b17578f1da6f5927360aad7a06f5fb52
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-01-09 10:34:32 -0800

    Added header to sign in/up page

--- salty-news/src/App.js
+++ salty-news/src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Page />
-      //<About></About>
+        <About></About>
       <HomePage />
     </div>
   );
--- salty-news/src/login_signup_page.js
+++ salty-news/src/login_signup_page.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Header from './components/Header'
 
 export const Page = () => {
   return (
     <div>
+      <Header />
       <Login />
       <Signup />
     </div>
