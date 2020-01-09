fd478aa3ae09205bb0cc420cdba54c80144b2ec2
Veto Ramirez <vetorobert3@gmail.com>
2020-01-08 21:11:23 -0800

    pulled from master and made some code for the comment card.

--- salty-news/src/components/CommentCard.js
+++ salty-news/src/components/CommentCard.js
@@ -9,9 +9,19 @@ export default function CommentCard(props) {
 
   const [comment, setComment] = useState({title: "", text: ""});
 
+  const handleChange = e => {
+    setNote({ ...comment, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.addCommentFn(comment);
+    setComment({ title: "", body: "" });
+  };
+
   return (
     <StyledCard>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label>
             title:
@@ -19,6 +29,18 @@ export default function CommentCard(props) {
               type='text' 
               name='title'
               value={title}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            text:
+            <input 
+              type='textarea' 
+              name='text'
+              value={text}
+              onChange={handleChange}
             />
           </label>
         </div>
