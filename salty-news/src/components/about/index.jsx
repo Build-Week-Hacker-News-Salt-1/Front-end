44bb46c756d95d51dc00967535c4a3c45eeed256
BenHall-7 <benjaminjahall@gmail.com>
2020-01-10 19:21:44 -0500

    Rework about page styles

--- salty-news/src/components/about/index.jsx
+++ salty-news/src/components/about/index.jsx
@@ -2,22 +2,31 @@ import React from 'react';
 // import { Card, Header } from "semantic-ui-react";
 import Team from './team';
 import styled from 'styled-components';
+import Header from '../Header';
 
 let Card = styled.div`
-    margin: 0 20px;
+    margin: 0 20px 10px;
+    background: lightgray;
 
     img {
         width: 250px;
         height: 250px;
         object-fit: cover;
     }
+
+    .padded {
+        padding: 0 15px 5px;
+    }
 `;
 
 const About = () => {
     return (
         <div>
+            {/* {<Header />} */}
             <h1 id="header" style={{
+                margin: "0 0 20px",
                 textAlign: "center",
+                background: "grey",
             }}>Hacker News Saltiest Comments Team</h1>
 
             <div style={{
@@ -30,9 +39,11 @@ const About = () => {
                     return (
                         <Card key={name}>
                             <img src={img} alt={name}/>
-                            <h2>{name}</h2>
-                            <h3>{role}</h3>
-                            <p>{bio || "LambdaSchool Student"}</p>
+                            <div className="padded">
+                                <h2>{name}</h2>
+                                <h3>{role}</h3>
+                                <p>{bio || "LambdaSchool Student"}</p>
+                            </div>
                         </Card>
                     )
                 })}
